cb9d782062cb92a7297a87aedf5902b107b9a25b
veiderpol2 <57240202+veiderpol2@users.noreply.github.com>
2019-11-13 16:42:27 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup(){
   resultImg = createImg('')
   resultImg.hide()
 
-  style = ml5.styleTransfer('models/animal', video, modelLoaded) 
+  style = ml5.styleTransfer('models/pantone', video, modelLoaded) 
 }
 function draw(){
   image(resultImg,0, 0, 800, 600)
